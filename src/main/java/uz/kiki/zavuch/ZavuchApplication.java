7b4e06aade280efa4f18ce7d26bb5b34fda4b156
Laziz Rohataliyev <rohataliyevlaziz@gmail.com>
2022-10-19 12:42:33 +0500

    initial commit

---
+++ src/main/java/uz/kiki/zavuch/ZavuchApplication.java
@@ -0,0 +1,13 @@
+package uz.kiki.zavuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavuchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZavuchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/kiki/zavuch/ZavuchApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.kiki.zavuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavuchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
